5d4a6b43e177e3cde6e3c4f4e812a427facd5c0c
idacbca <idacbca@126.comm>
2020-01-21 00:18:02 +0800

    Improved the performance of ArrayDeque

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -8,12 +8,29 @@ public class ArrayDeque<T> {
     public ArrayDeque() {
         items = (T[]) new Object[8];
         size = 0;
+        nextFirst = 0;
+        nextLast = 1;
+    }
+
+    /** Makes nextFirst/nextLast pointing at next position. */
+    private int forward(int p) {
+        return (p + 1) % items.length;
+    }
+
+    /** Makes nextFirst/nextLast pointing at previous position. */
+    private int back(int p) {
+        /** Adds an additional item.length since p - 1 could be negative. */
+        return (p - 1 + items.length) % items.length;
     }
 
     /** Resize the underlying array to the target capacity. */
     private void resize(int capacity) {
         T[] a = (T[]) new Object[capacity];
-        System.arraycopy(items, 0, a, 0, size);
+        int p = forward(nextFirst);
+        for (int i = 0; i < size; i ++){
+            a[i] = items[p];
+            p = forward(p);
+        }
         items = a;
     }
 
@@ -26,25 +43,22 @@ public class ArrayDeque<T> {
 
     /** Adds an item of type T to the front of the deque. */
     public void addFirst(T x) {
-        if (size == 0) {
-            items[0] = x;
-            size = size + 1;
-        } else {
-            T[] a = (T[]) new Object[items.length + 1];
-            System.arraycopy(items, 0, a, 1, size);
-            a[0] = x;
-            items = a;
-            size = size + 1;
-            resize();
+        if (size == items.length) {
+            resize(size * 2);
         }
+        items[nextFirst] = x;
+        nextFirst = back(nextFirst);
+        size = size + 1;
+        resize();
     }
 
     /** Adds an item of type T to the back of the deque. */
     public void addLast(T x) {
         if (size == items.length) {
-            resize(size + 1);
+            resize(size * 2);
         }
-        items[size] = x;
+        items[nextLast] = x;
+        nextLast = forward(nextLast);
         size = size + 1;
         resize();
     }
@@ -61,7 +75,7 @@ public class ArrayDeque<T> {
 
     /** Prints the items in the deque from first to last, separated by a space. */
     public void printDeque() {
-        for (int i = 0; i < size; i++) {
+        for (int i = forward(nextFirst); i < size; i = forward(i)) {
             System.out.print(items[i] + " ");
         }
         System.out.println("");
@@ -71,10 +85,9 @@ public class ArrayDeque<T> {
      * If no such item exists, returns null. */
     public T removeFirst() {
         if (size != 0) {
-            T x = get(0);
-            T[] a = (T[]) new Object[items.length];
-            System.arraycopy(items, 1, a, 0, size - 1);
-            items = a;
+            nextFirst = forward(nextFirst);
+            T x = get(nextFirst);
+            items[nextFirst] = null;
             size = size - 1;
             resize();
             return x;
@@ -87,8 +100,9 @@ public class ArrayDeque<T> {
      * If no such item exists, returns null. */
     public T removeLast() {
         if (size != 0) {
-            T x = get(size - 1);
-            items[size - 1] = null;
+            nextLast = back(nextLast);
+            T x = get(nextLast);
+            items[nextLast] = null;
             size = size - 1;
             resize();
             return x;
