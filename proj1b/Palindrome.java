9b3cf159fecd94d1e826ddad3df56adf72a3e5d7
idacbca <idacbca@126.comm>
2020-01-23 22:02:48 +0800

    Completed task 1 to task 4.

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,10 +1,46 @@
 public class Palindrome {
+    /** Transfer and returns a String to a deque in the same order
+     * as a String. */
     public Deque<Character> wordToDeque(String word) {
-        char wordArray[] = word.toCharArray();
+        char[] wordArray = word.toCharArray();
         LinkedListDeque<Character> lld = new LinkedListDeque<>();
-        for (int i = 0; i < wordArray.length; i++) {
-            lld.addLast(wordArray[i]);
+        for (char c : wordArray) {
+            lld.addLast(c);
         }
         return lld;
+    }
+
+    /** A helper method of isPalindrome. */
+    private boolean isPalindrome(Deque<Character> pDeque) {
+        if (pDeque.size() == 1 || pDeque.size() == 0) {
+            return true;
+        }
+        if (pDeque.removeFirst() != pDeque.removeLast()) {
+            return false;
+        }
+        return isPalindrome(pDeque);
+    }
+
+    /** Checks if a word is a palindrome and returns true/false. */
+    public boolean isPalindrome(String word) {
+        Deque<Character> pDeque = wordToDeque(word);
+        return isPalindrome(pDeque);
+    }
+
+    /** A helper method of off-by-one isPalindrome. */
+    private boolean isPalindrome(Deque<Character> pDeque, CharacterComparator cc) {
+        if (pDeque.size() == 1 || pDeque.size() == 0) {
+            return true;
+        }
+        if (!cc.equalChars(pDeque.removeFirst(), pDeque.removeLast())) {
+            return false;
+        }
+        return isPalindrome(pDeque, cc);
+    }
+
+    /** Checks if a word is an off-by-one palindrome and returns true/false. */
+    public boolean isPalindrome(String word, CharacterComparator cc) {
+        Deque<Character> pDeque = wordToDeque(word);
+        return isPalindrome(pDeque, cc);
     }
 }
--- proj1b/TestPalindrome.java
+++ proj1b/TestPalindrome.java
@@ -15,4 +15,39 @@ public class TestPalindrome {
         }
         assertEquals("persiflage", actual);
     }
+
+    @Test
+    public void testIsPalindrome() {
+        String A = "a";
+        String B = "racecar";
+        String C = "horse";
+        String D = "Racecar";
+        String E = "";
+
+        assertTrue(palindrome.isPalindrome(A));
+        assertTrue(palindrome.isPalindrome(B));
+        assertFalse(palindrome.isPalindrome(C));
+        assertFalse(palindrome.isPalindrome(D));
+        assertTrue(palindrome.isPalindrome(E));
+    }
+
+    @Test
+    public void testIsPalindromeCC() {
+        CharacterComparator offByOne = new OffByOne();
+
+        String A = "a";
+        assertTrue(palindrome.isPalindrome(A, offByOne));
+
+        String B = "qadecbr";
+        assertTrue(palindrome.isPalindrome(B, offByOne));
+
+        String C = "&%b&%";
+        assertTrue(palindrome.isPalindrome(C, offByOne));
+
+        String D = "askhdh";
+        assertFalse(palindrome.isPalindrome(D, offByOne));
+
+        String E = "qadcbm";
+        assertFalse(palindrome.isPalindrome(E, offByOne));
+    }
 }
