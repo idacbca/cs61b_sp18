ed1ad673eb44c632b6c11a97246b1c9ea9a9bd3f
acbca <idacbca@126.com>
2020-12-27 15:12:14 +0800

    Completed Lab1.

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -14,18 +14,22 @@ public class LeapYear {
         }
     }
 
+    public static boolean isLeapYear(int year) {
+        return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
+    }
+
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
             System.out.println("Please enter command line arguments.");
             System.out.println("e.g. java Year 2000");
         }
-        for (int i = 0; i < args.length; i++) {
+        for (String arg : args) {
             try {
-                int year = Integer.parseInt(args[i]);
+                int year = Integer.parseInt(arg);
                 checkLeapYear(year);
             } catch (NumberFormatException e) {
-                System.out.printf("%s is not a valid number.\n", args[i]);
+                System.out.printf("%s is not a valid number.\n", arg);
             }
         }
     }
